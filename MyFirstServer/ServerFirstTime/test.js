b323cfdf933b6c77d057e72ef4d84f57a011c9bb
RasmusGyllenhammar <rasmus.gyllenhammar@elev.ga.ntig.se>
2020-09-10 13:04:38 +0200

    uppgift 2, post

--- MyFirstServer/ServerFirstTime/test.js
+++ MyFirstServer/ServerFirstTime/test.js
@@ -4,9 +4,10 @@ const port = 3001
 
 const clientDir = __dirname + "\\client\\"
 
+app.use(express.json())
+app.use(express.urlencoded())
 
-
-app.get('/', (req, res) => res.sendfile(clientDir + "index.html"))
+app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
 
 app.get('/mane', (req, res) => {
   res.sendFile(clientDir + "maneprofile.jpg")
@@ -16,6 +17,13 @@ app.get('/style', (req, res) => {
   res.sendFile(clientDir + "style.css")
 })
 
+app.post('/', function (req, res) {
+  console.log(req.body.name)
+  console.log(req.body.email)
+  res.redirect('/')
+ 
+})
 
 app.get('/teknik', (req, res) => res.send('ECONOMY'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
